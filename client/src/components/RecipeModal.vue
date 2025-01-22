2c2983285927a6254b9b07fe3abc8d9f8e5be8d5
Mike Nurisio <michaelnurisio@gmail.com>
2025-01-22 13:23:10 -0700

    delete recipe

--- client/src/components/RecipeModal.vue
+++ client/src/components/RecipeModal.vue
@@ -1,56 +1,77 @@
 <script setup>
 import { AppState } from '@/AppState';
-import { Recipe } from '@/models/Recipe';
+import { recipeService } from '@/services/RecipeService';
+import Pop from '@/utils/Pop';
+import { Modal } from 'bootstrap';
 import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
 
 
 
 const recipe = computed(() => AppState.activeRecipe)
-const ingredients = computed(()=> AppState.ingredients)
+const ingredients = computed(() => AppState.ingredients)
 
 
+async function deleteRecipe() {
+    try {
+        const confirm = await Pop.confirm("are you sure you want to delete this recipe?")
+        if(!confirm) return
+        await recipeService.deleteRecipe()
+
+        Modal.getInstance('#recipeModal').hide()
+    }
+    catch (error) {
+        Pop.error(error);
+    }
+}
 
 
 </script>
 
 
 <template>
-    <div class="container-fluid modal fade " id="recipeModal" tabindex="-1" aria-labelledby="recipeModalLabel" aria-hidden="true">
+    <div class="container-fluid modal fade " id="recipeModal" tabindex="-1" aria-labelledby="recipeModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div v-if="recipe" class="modal-content">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <img class="img-fluid w-100 h-100" :src="recipe.img" :alt="recipe.title">
+                <div class="row">
+                    <div class="col-md-6">
+                        <img class="img-fluid w-100 h-100" :src="recipe.img" :alt="recipe.title">
+                    </div>
+                    <div class="col-6">
+                        <div class="row">
+                            <span role="button" data-bs-dismiss="modal" class="text-end">
+                                <i class="mdi mdi-close-thick p-2"></i>
+                            </span>
                         </div>
-                        <div class="col-6">
-                            <div class="row">
-                                <span role="button" data-bs-dismiss="modal" class="text-end">
-                                    <i class="mdi mdi-close-thick p-2"></i>
-                                </span>
-                            </div>
-                            <div class="fs-3 text-success">
+                        <div>
+                            <span class="fs-3 text-success">
                                 {{ recipe.title }}
+                            </span>
+                            <span><button @click="deleteRecipe()" class="btn btn-danger ms-2 rounded rounded-pill">Delete</button></span>
+                        </div>
+                        <div>
+                            by: {{ recipe.creator.name }}
+                            <div class="my-md-2">
+                                <span class="rounded rounded-pill bg-secondary text-light py-md-1 px-md-2">{{
+                                    recipe.category }}</span>
                             </div>
-                            <div>
-                                by: {{ recipe.creator.name }}
-                                <div class="my-md-2">
-                                    <span class="rounded rounded-pill bg-secondary text-light py-md-1 px-md-2">{{ recipe.category }}</span>
-                                </div>
-                            </div>
-                            <div>
-                                <h5>Ingredients</h5>
-                            </div>
-                            <div v-for="ingredient in ingredients" :key="ingredient.id">
-                                <p>{{ ingredient.quantity }}  {{ ingredient.name }}</p>
-                            </div>
-                            <div>
-                                <h5>Instructions</h5>
-                            </div>
-                            <div>
-                                <p>{{ recipe.instructions }}</p>
-                            </div>
+                        </div>
+                        <div>
+                            <h5>Ingredients</h5>
+                        </div>
+                        <div v-for="ingredient in ingredients" :key="ingredient.id">
+                            <p>{{ ingredient.quantity }} {{ ingredient.name }}</p>
+                        </div>
+                        <div>
+                            <h5>Instructions</h5>
+                        </div>
+                        <div>
+                            <p>{{ recipe.instructions }}</p>
                         </div>
                     </div>
+                </div>
             </div>
         </div>
     </div>
@@ -63,7 +84,7 @@ const ingredients = computed(()=> AppState.ingredients)
     object-position: center;
 }
 
-.categoryName{
+.categoryName {
     background-color: #ffffffa4;
 }
 </style>
--- client/src/services/RecipeService.js
+++ client/src/services/RecipeService.js
@@ -5,7 +5,14 @@ import { AppState } from "@/AppState.js"
 
 
 class RecipeService {
-   async createRecipe(recipeData) {
+    async deleteRecipe() {
+        const response = AppState.activeRecipe
+        logger.log('deleting recipe', response)
+        const recipeToDelete = AppState.recipe.indexOf(response)
+        AppState.recipe.splice(recipeToDelete, 1)
+        AppState.activeRecipe = null
+    }
+    async createRecipe(recipeData) {
         const response = await api.post('api/recipes', recipeData)
         logger.log('Created Recipe', response.data)
         const recipe = new Recipe(response.data)
